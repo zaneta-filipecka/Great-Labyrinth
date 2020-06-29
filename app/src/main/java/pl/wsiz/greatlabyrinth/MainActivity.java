d26f4745aa908762d43b3d3330895018c82c706c
Żaneta Filipecka <zaneta_filipecka@interia.eu>
2020-06-29 14:40:51 +0200

    image

--- app/src/main/java/pl/wsiz/greatlabyrinth/MainActivity.java
+++ app/src/main/java/pl/wsiz/greatlabyrinth/MainActivity.java
@@ -6,16 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button playButton;
     private Button exitButton;
+    private ImageView labyrinth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        labyrinth = (ImageView)findViewById(R.id.labyrinth);
 
         playButton = (Button)findViewById(R.id.playButton);
         playButton.setOnClickListener(new View.OnClickListener() {
